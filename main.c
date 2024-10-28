e88b22bf436c55fb1b514115fdcf5e3a15fc7211
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2024-10-29 00:15:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 int main (void) {
 	chessboard *chs = create_chessboard ();
 	print_chessboard (chs);
-	printf("\n");
+	printf ("\n");
 	char input[1000];
 	scanf ("%[^\n]s", input);
-	printf("\n");
 	play_piece (input, chs);
+	printf ("\n");
 	print_chessboard (chs);
 	free_chessboard (chs);
 
